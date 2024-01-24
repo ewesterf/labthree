b5d6d1f6612946f90bb28336e2161c445cdfcbd7
Ema Nichole Westerfeld <ewesterf@purdue.edu>
2024-01-24 12:35:25 -0500

    complete

--- labthree/labthree.cpp
+++ labthree/labthree.cpp
@@ -3,9 +3,89 @@
 
 #include <iostream>
 
+using namespace std;
+
+void printMenu(int& choice)
+{
+    cout << "Welcome to my program, please select and operation to perform:" << endl;
+    cout << "1. Addition" << endl;
+    cout << "2. Subtraction" << endl;
+    cout << "3. Multiplication" << endl;
+    cout << "4. Division" << endl;
+
+    cout << "\nYour Selection: ";
+    cin >> choice;
+}
+
+void getChoices(float& A, float& B)
+{
+    cout << "Please enter the first value:";
+    cin >> A;
+    cout << "Please enter the second value:";
+    cin >> B;
+    // The rest of this function is an exercise to the reader
+}
+
+void addition(float A, float B)
+{
+    float X;
+    cout << "Solving " << A << " + " << B << " = ..." << endl;
+    X = A + B;
+    cout << "The answer is " << X;
+}
+
+void subtraction(float A, float B)
+{
+    float X;
+    cout << "Solving " << A << " - " << B << " = ..." << endl;
+    X = A - B;
+    cout << "The answer is " << X;
+}
+
+void multiplication(float A, float B)
+{
+    float X;
+    cout << "Solving " << A << " * " << B << " = ..." << endl;
+    X = A * B;
+    cout << "The answer is " << X;
+}
+
+void division(float A, float B)
+{
+    float X;
+    cout << "Solving " << A << " / " << B << " = ..." << endl;
+    X = A / B;
+    cout << "The answer is " << X;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    int choice;
+    float A;
+    float B;
+
+    printMenu(choice);
+    getChoices(A, B);
+
+    if (choice == 1)
+    {
+        addition(A, B);
+    }
+    if (choice == 2)
+    {
+        subtraction(A, B);
+    }
+    if (choice == 3)
+    {
+        multiplication(A, B);
+    }
+    if (choice == 4)
+    {
+        division(A, B);
+    }
+
+    return 0;
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
